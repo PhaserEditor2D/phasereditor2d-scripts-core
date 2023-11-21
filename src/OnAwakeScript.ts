0fd1219bfdefc377daa265d2f4d91234f1d3d7e1
PhaserEditor2D <developers@phasereditor2d.com>
2023-11-21 04:20:30 -0500

    Adds AwakeScript.

---
+++ src/OnAwakeScript.ts
@@ -0,0 +1,33 @@
+
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+import ScriptNode from "./ScriptNode";
+import Phaser from "phaser";
+/* START-USER-IMPORTS */
+/* END-USER-IMPORTS */
+
+export default class OnAwakeScript extends ScriptNode {
+
+	constructor(parent: ScriptNode | Phaser.GameObjects.GameObject | Phaser.Scene) {
+		super(parent);
+
+		/* START-USER-CTR-CODE */
+		this.scene.events.once("scene-awake", () => {
+
+			this.executeChildren();
+		});
+		/* END-USER-CTR-CODE */
+	}
+
+	/* START-USER-CODE */
+
+	// Write your code here.
+
+	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
---
+++ types/OnAwakeScript.d.ts
@@ -0,0 +1,5 @@
+import ScriptNode from "./ScriptNode";
+import Phaser from "phaser";
+export default class OnAwakeScript extends ScriptNode {
+    constructor(parent: ScriptNode | Phaser.GameObjects.GameObject | Phaser.Scene);
+}
