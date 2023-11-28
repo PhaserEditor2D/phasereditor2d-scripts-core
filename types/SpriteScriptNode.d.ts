1bc73352ca02910e85d26f372d3c0855dd77d370
PhaserEditor2D <developers@phasereditor2d.com>
2023-11-28 12:24:32 -0500

    Build.

--- types/SpriteScriptNode.d.ts
+++
@@ -1,6 +0,0 @@
-import ScriptNode from "./ScriptNode";
-import Phaser from "phaser";
-export default class SpriteScriptNode extends ScriptNode {
-    constructor(parent: ScriptNode | Phaser.GameObjects.GameObject | Phaser.Scene);
-    get gameObject(): Phaser.GameObjects.Sprite;
-}
